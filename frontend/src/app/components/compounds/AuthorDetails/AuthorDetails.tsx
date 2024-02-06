f0a70a04ee2f0e44ff25a9e8e33788191de052a9
MohammadAun <73369791+MohammadAun@users.noreply.github.com>
2024-02-06 15:35:19 -0600

    Update AuthorDetails.tsx

--- frontend/src/app/components/compounds/AuthorDetails/AuthorDetails.tsx
+++ frontend/src/app/components/compounds/AuthorDetails/AuthorDetails.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { Paper, Typography } from '@mui/material';
 
 interface AuthorDetailsProps {
-    firstName: string;
-    lastName: string;
-    dateOfBirth: Date;
+    authorName: string;
+    birthYear: number;
     homeTown: string;
     bio: string;
 }
@@ -12,8 +11,8 @@ interface AuthorDetailsProps {
 const AuthorDetails = ({ firstName, lastName, dateOfBirth, homeTown, bio }) => {
     return (
         <Paper variant="outlined" style={{ padding: 20 }}>
-            <Typography variant="h5">Author: {firstName + lastName}</Typography>
-            <Typography variant="body1">Date Of Birth: {dateOfBirth}</Typography>
+            <Typography variant="h5">Author: {authorName}</Typography>
+            <Typography variant="body1">Date Of Birth: {birthYear}</Typography>
             <Typography variant="body1">Home Town: {homeTown}</Typography>
             <Typography variant="body1">Bio: {bio}</Typography>
         </Paper>
