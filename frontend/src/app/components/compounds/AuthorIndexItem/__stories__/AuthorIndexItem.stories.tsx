b44cf720f339184bf2fbae7b8d9c406d93fd79ee
Duncan Beyer <84360347+duncanbeyer@users.noreply.github.com>
2024-02-08 11:07:31 -0600

    Duncan/g4 27 author index item (#7)
    * Created AuthorIndexItem component based on miniproject components.
    
    * Ran yarn to install front and backend dependencies.
    
    * Updated some MUI
    
    * Storybook written and working
    
    * Story created and working.
    
    * MUI displaying full name and no title now.
    
    * linter change.
    
    * Updated item, removing formik, edit/delete capabilities. Small changes like authorId made as well.
    
    * added build folder to gitignore and ran command to remove build folder from git
    
    * updated gitignore

---
+++ frontend/src/app/components/compounds/AuthorIndexItem/__stories__/AuthorIndexItem.stories.tsx
@@ -0,0 +1,26 @@
+import Paper from '@mui/material/Paper';
+
+import AuthorIndexItem from '..';
+import { Grid } from '@mui/material';
+
+export default {
+    title: 'Compounds/AuthorIndexItem',
+    component: AuthorIndexItem,
+};
+
+export const Basic = (args) => {
+    return (
+        <Grid container>
+            <Grid item xs={12} sm={9} lg={6}>
+                <Paper variant="outlined" sx={{ p: 4, background: '#ffffee' }}>
+                    <AuthorIndexItem
+                        authorId="12345"
+                        authorName="Herman Melville"
+                        birthYear={1819}
+                        hometown="Nantucket"
+                    />
+                </Paper>
+            </Grid>
+        </Grid>
+    );
+};
