440ecc73c8631f90e11b87ef845b8eec3fb40657
ekrzysz <EKRZYSZ1@depaul.edu>
2024-02-09 15:16:26 -0600

    jwt now expires in 10 hours

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { AuthGuard } from './auth.guard';
         JwtModule.register({
             global: true,
             secret: jwtConstants.secret,
-            signOptions: { expiresIn: '60s' },
+            signOptions: { expiresIn: '10h' },
         }),
     ],
     providers: [AuthService, AuthResolver, AuthGuard],
