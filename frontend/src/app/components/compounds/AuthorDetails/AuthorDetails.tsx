8d3f214f02f98111febad555cde76f3716280639
MohammadAun <73369791+MohammadAun@users.noreply.github.com>
2024-02-09 19:44:13 -0600

    Create AuthorDetails.tsx

---
+++ frontend/src/app/components/compounds/AuthorDetails/AuthorDetails.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Paper, Typography } from '@mui/material';
+
+interface AuthorDetailsProps {
+    name: string;
+    birthYear: number;
+    homeTown: string;
+    bio: string;
+}
+
+const AuthorDetails = ({ firstName, lastName, dateOfBirth, homeTown, bio }) => {
+    return (
+        <Paper variant="outlined" style={{ padding: 20 }}>
+            <Typography variant="h5">Author: {authorName}</Typography>
+            <Typography variant="body1">Date Of Birth: {birthYear}</Typography>
+            <Typography variant="body1">Home Town: {homeTown}</Typography>
+            <Typography variant="body1">Bio: {bio}</Typography>
+        </Paper>
+    );
+};
+
+export default AuthorDetails;
