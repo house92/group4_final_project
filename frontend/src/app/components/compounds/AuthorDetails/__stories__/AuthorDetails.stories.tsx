4efb6b2e823faf4a78016f2eb100b95846a7a4f2
MohammadAun <73369791+MohammadAun@users.noreply.github.com>
2024-02-09 19:44:10 -0600

    Create AuthorDetails.stories.tsx

---
+++ frontend/src/app/components/compounds/AuthorDetails/__stories__/AuthorDetails.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Paper, Typography } from '@mui/material';
+import AuthorDetails from '../AuthorDetails';
+
+export default {
+    title: 'Compounds/AuthorDetails',
+};
+
+export const Default = () => (
+    <AuthorDetails 
+        name="John Doe"
+        birthYear="2002"
+        homeTown="Chicago"
+        bio="This is the bio..."
+    />
+);
