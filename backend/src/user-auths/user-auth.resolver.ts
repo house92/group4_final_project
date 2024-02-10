1a864914c616f7fdde2c66eea22bb8fc2187a7c0
Sam <samuel.house@ntlworld.com>
2024-02-10 16:00:37 -0600

    cleaned up user auth resolver

--- backend/src/user-auths/user-auth.resolver.ts
+++ backend/src/user-auths/user-auth.resolver.ts
@@ -1,18 +1,10 @@
-import { Resolver, Args, Mutation } from '@nestjs/graphql';
+import { Resolver } from '@nestjs/graphql';
 import { Public } from 'src/auth/decorators/public.decorator';
 import { UserAuthService } from './user-auth.service';
-import { CreateUserAuthInput } from './inputs/create-user-auth-input';
-import { UserAuth } from './user-auth.entity';
 //import { AuthGuard } from './auth.guard';
 
 @Public()
 @Resolver()
 export class UserAuthResolver {
     constructor(private readonly service: UserAuthService) {}
-
-    @Public()
-    @Mutation(() => UserAuth)
-    async registerUser(@Args('input') input: CreateUserAuthInput) {
-        return this.service.createUser(input);
-    }
 }
