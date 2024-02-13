1aa5075b260d0274c1665d33ec7ed4975f83bb8f
MohammadAun <73369791+MohammadAun@users.noreply.github.com>
2024-02-13 16:40:56 -0600

    Update SigninPage.tsx

--- frontend/src/app/pages/SignInPage/SigninPage.tsx
+++ frontend/src/app/pages/SignInPage/SigninPage.tsx
@@ -1,8 +1,23 @@
 import SignInForm from "app/components/compounds/SignInForm";
+
+import { useParams } from 'react-router-dom';
 import useSignIn from "./useSignIn";
 
 export default function SignInPage() {
-    const { user } = useSignIn();
 
-    return <SignInForm onSubmit={({ email, password }) => useSignIn({ variable: { email, password } })} />
+    const { userID } = useParams();
+
+    const { user } = useSignIn(email, password);
+
+    return (
+        <div>
+            <h1>Sign In Page</h1>
+            <div style={{ marginBottom: '20px' }}></div>
+            <SignInForm
+                email={user?.email}
+                password={user?.password}
+                onSubmit={({ email, password }) => useSignIn({ variable: { email, password } })} 
+            />
+        </div>
+    )
 }
