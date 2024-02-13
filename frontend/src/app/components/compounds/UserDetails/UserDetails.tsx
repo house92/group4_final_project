480747d6af427221053b91741056919ae8526739
Duncan Beyer <duncan011217@gmail.com>
2024-02-13 16:11:58 -0600

    updated

--- frontend/src/app/components/compounds/UserDetails/UserDetails.tsx
+++ frontend/src/app/components/compounds/UserDetails/UserDetails.tsx
@@ -3,18 +3,16 @@ import { Paper, Typography } from '@mui/material';
 
 interface UserDetailsProps {
     id: string;
-    firstName: string;
-    lastName: string;
+    name: string;
     bio: string;
     age: string;
 }
 
-const UserDetails = ({ id, firstName, lastName, bio, age }) => {
+const UserDetails = ({ id, name, bio, age }) => {
     return (
         <Paper variant="outlined" style={{ padding: 20 }}>
             <Typography variant="h5">{id}</Typography>
-            <Typography variant="body1">First Name: {firstName}</Typography>
-            <Typography variant="body1">Last Name: {lastName}</Typography>
+            <Typography variant="body1">Name: {name}</Typography>
             <Typography variant="body1">Bio: {bio}</Typography>
             <Typography variant="body1">Age: {age}</Typography>
         </Paper>
--- frontend/src/app/components/compounds/UserDetails/__stories__/UserDetails.stories.tsx
+++ frontend/src/app/components/compounds/UserDetails/__stories__/UserDetails.stories.tsx
@@ -9,8 +9,7 @@ export default {
 export const Default = () => (
     <UserDetails
         id="8004b2f0-fccc-42e2-942b-e298562bde97"
-        firstName="John"
-        lastName="Doe"
+        name="John Doe"
         bio="bio of user"
         age="20"
     />
