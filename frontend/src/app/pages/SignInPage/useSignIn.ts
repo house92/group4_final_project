827db1e16554677be42fad0ba2b1713558e79568
MohammadAun <73369791+MohammadAun@users.noreply.github.com>
2024-02-14 13:26:08 -0600

    Update useSignIn.ts

--- frontend/src/app/pages/SignInPage/useSignIn.ts
+++ frontend/src/app/pages/SignInPage/useSignIn.ts
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { useSignInUserMutation } from 'generated/graphql';
 
@@ -7,16 +9,30 @@ interface User {
 }
 
 export default function useSignIn() {
-    const { data } = useSignInUserMutation({ variables: { email, password }});
+    const [error, setError] = useState('');
+    const history = useHistory();
+    const [signInUserMutation] = useSignInUserMutation();
 
-    let user: User | undefined;
-    if (data?.signInUser) {
-        user = {
-            id: data?.signInUser.id,
-            firstName: data?.signInUser.firstName,
-            lastName: data?.signInUser.lastName,
-            token: data?.signInUser.token,
-        };
-    }
-    return { user };
+    const signIn = async (email: string, password: string) => {
+        try {
+            const {data } = await signInUserMutation({ variables: { email, password }});
+
+            if (data?.signInUser) {
+                const { id, firstName, lastName, token } = data.signInUser;
+                const user: User = { id, firstName, lastName, token };
+                history.push('/homePage');
+                return { user: data.signInUser, error: '' };
+            } else {
+                const errorMessage = 'Invalid email or password.';
+                setError(errorMessage);
+                return { user: null, error: errorMessage };
+              }
+        } catch (error) {
+            console.error('Error signing in:', error);
+            setError('An unexpected error occurred.');
+            return { user: null, error: 'An unexpected error occurred.' };
+        }
+    };
+
+    return { signIn, error };
 }
