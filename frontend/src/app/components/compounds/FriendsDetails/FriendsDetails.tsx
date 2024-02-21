4887844bc43a753bab416b1419d7121399465b73
MohammadAun <73369791+MohammadAun@users.noreply.github.com>
2024-02-20 23:58:22 -0600

    Update FriendsDetails.tsx

--- frontend/src/app/components/compounds/FriendsDetails/FriendsDetails.tsx
+++ frontend/src/app/components/compounds/FriendsDetails/FriendsDetails.tsx
@@ -3,16 +3,12 @@ import { Paper, Typography } from '@mui/material';
 
 interface FriendsDetailsProps {
     name: string;
-    profilePic: string;
-    bio: string;
 }
 
 const FriendsDetails = ({ name, profilePic, bio }) => {
     return (
         <Paper variant="outlined" style={{ padding: 20 }}>
             <Typography variant="h5">Name: {name}</Typography>
-            <img src={profilePic} alt="Profile Picture:" style={{ maxWidth: '100%', marginBottom: 10 }} />
-            <Typography variant="body1">Bio: {bio}</Typography>
         </Paper>
     );
 };
