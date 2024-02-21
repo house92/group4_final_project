8719da820a7d196c1f0c830bd5fae7d879b64008
MohammadAun <73369791+MohammadAun@users.noreply.github.com>
2024-02-20 23:58:17 -0600

    Update FriendsDetails.stories.tsx

--- frontend/src/app/components/compounds/FriendsDetails/__stories__/FriendsDetails.stories.tsx
+++ frontend/src/app/components/compounds/FriendsDetails/__stories__/FriendsDetails.stories.tsx
@@ -7,7 +7,5 @@ export default {
 export const Default = () => (
     <FriendsDetails
         name="Friend Name"
-        profilePic="Prolfile Picture URL"
-        bio="This is the friends bio..."
     />
 );
