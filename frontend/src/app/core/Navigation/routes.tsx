aa3352db3ae0268e3e169ad53fcbbd1d4073d0d8
MohammadAun <73369791+MohammadAun@users.noreply.github.com>
2024-02-21 15:22:57 -0600

    Update routes.tsx

--- frontend/src/app/core/Navigation/routes.tsx
+++ frontend/src/app/core/Navigation/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { AuthorIndexPage } from 'app/pages';
 import AuthorPage from 'app/pages/AuthorPage';
 import { Layout } from 'app/components';
+import SignInPage from 'app/pages/SignInPage';
 
 export function generateRouter() {
     return createBrowserRouter([
@@ -17,6 +18,10 @@ export function generateRouter() {
                     path: 'authors/:authorId',
                     element: <AuthorPage />,
                 },
+                {
+                    path: 'signin',
+                    element: <SignInPage />,
+                }
             ],
         },
     ]);
