ac7cf05c6d437953fd0a05f379922c6875e98000
MohammadAun <73369791+MohammadAun@users.noreply.github.com>
2024-02-23 10:41:52 -0600

    Aun/g4 40/create authentication page (#24)
    * Create LoginForm.tsx
    
    * Create index.ts
    
    * Create AuthorDetails.stories.tsx
    
    * Create AuthorDetails.tsx
    
    * Update LoginForm.tsx
    
    * Update AuthorDetails.tsx
    
    * Delete index.ts
    
    * Delete LoginForm.tsx
    
    * Create index.ts
    
    * Create SignInForm.tsx
    
    * Update index.ts
    
    * Update SignInForm.tsx
    
    * Update package.json
    
    * Update yarn.lock
    
    * Create SigninForm.stories.tsx
    
    * Update SignInForm.tsx
    
    * Create SigninPage.tsx
    
    * Create operation.gql
    
    * Update SigninPage.tsx
    
    * Delete operation.gql
    
    * Delete SigninPage.tsx
    
    * Create operation.gql
    
    * Create SigninPage.tsx
    
    * Create index.ts
    
    * Update operation.gql
    
    * Update SigninPage.tsx
    
    * Create useSignIn.ts
    
    * Update operation.gql
    
    * Update SigninPage.tsx
    
    * Update operation.gql
    
    * Update useSignIn.ts
    
    * Update SigninPage.tsx
    
    * Update useSignIn.ts
    
    * Update SigninPage.tsx
    
    * Update SignInForm.tsx
    
    * Update operation.gql
    
    * Update SigninPage.tsx
    
    * Update useSignIn.ts
    
    * Update graphql.tsx
    
    * Update frontend/src/app/pages/SignInPage/SigninPage.tsx
    
    Co-authored-by: Sam <samuel.house@ntlworld.com>
    
    * Update frontend/src/app/pages/SignInPage/SigninPage.tsx
    
    Co-authored-by: Sam <samuel.house@ntlworld.com>
    
    * Update frontend/src/app/pages/SignInPage/SigninPage.tsx
    
    Co-authored-by: Sam <samuel.house@ntlworld.com>
    
    * Update index.ts
    
    * Update SigninPage.tsx
    
    * Update useSignIn.ts
    
    * Update index.ts
    
    * Update index.ts
    
    * Update routes.tsx
    
    * Update SigninPage.tsx
    
    * Update useSignIn.ts
    
    * Update graphql.tsx
    
    * Update SigninPage.tsx
    
    * Update useSignIn.ts
    
    * Update routes.tsx
    
    * Update SigninPage.tsx
    
    * Update useSignIn.ts
    
    * Update routes.tsx
    
    * Update useSignIn.ts
    
    * Update routes.tsx
    
    * Update useSignIn.ts
    
    * Got Sign-In page to work
    
    ---------
    
    Co-authored-by: Sam <samuel.house@ntlworld.com>

--- frontend/src/app/core/Navigation/routes.tsx
+++ frontend/src/app/core/Navigation/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { AuthorIndexPage, AuthorPage, BookIndexPage, BookPage } from 'app/pages';
 import { Layout } from 'app/components';
+import SignInPage from 'app/pages/SignInPage';
 
 export function generateRouter() {
     return createBrowserRouter([
@@ -15,6 +16,10 @@ export function generateRouter() {
                 {
                     path: 'authors/:authorId',
                     element: <AuthorPage />,
+                },
+                {
+                    path: 'sign-in',
+                    element: <SignInPage />,
                 },
                 {
                     path: 'books',
--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -126,6 +126,7 @@ export type CreateUserAuthInput = {
 
 export type Mutation = {
   __typename?: 'Mutation';
+  addFriend: User;
   createAuthor: Author;
   createBook: Book;
   createBookReview: BookReview;
@@ -138,6 +139,11 @@ export type Mutation = {
   updateAuthor: Author;
   updateBook: Book;
   updateUser: User;
+};
+
+
+export type MutationAddFriendArgs = {
+  friendId: Scalars['String']['input'];
 };
 
 
@@ -296,6 +302,7 @@ export type User = {
   bio?: Maybe<Scalars['String']['output']>;
   dateOfBirth: Scalars['String']['output'];
   firstName: Scalars['String']['output'];
+  friends?: Maybe<Array<User>>;
   id: Scalars['ID']['output'];
   lastName: Scalars['String']['output'];
   userAuth: UserAuth;
@@ -344,6 +351,14 @@ export type GetBookByIdQueryVariables = Exact<{
 
 
 export type GetBookByIdQuery = { __typename?: 'Query', getBook: { __typename?: 'Book', id: string, title: string, coverImage: string, publicationDate?: string | null, synopsis?: string | null, authors: Array<{ __typename?: 'Author', firstName?: string | null, lastName: string }> } };
+
+export type SignInUserMutationVariables = Exact<{
+  email: Scalars['String']['input'];
+  password: Scalars['String']['input'];
+}>;
+
+
+export type SignInUserMutation = { __typename?: 'Mutation', signInUser: { __typename?: 'UserSession', id: string, firstName: string, lastName: string } };
 
 
 export const GetUserSessionDocument = gql`
@@ -571,3 +586,39 @@ export type GetBookByIdQueryHookResult = ReturnType<typeof useGetBookByIdQuery>;
 export type GetBookByIdLazyQueryHookResult = ReturnType<typeof useGetBookByIdLazyQuery>;
 export type GetBookByIdSuspenseQueryHookResult = ReturnType<typeof useGetBookByIdSuspenseQuery>;
 export type GetBookByIdQueryResult = Apollo.QueryResult<GetBookByIdQuery, GetBookByIdQueryVariables>;
+export const SignInUserDocument = gql`
+    mutation SignInUser($email: String!, $password: String!) {
+  signInUser(email: $email, password: $password) {
+    id
+    firstName
+    lastName
+  }
+}
+    `;
+export type SignInUserMutationFn = Apollo.MutationFunction<SignInUserMutation, SignInUserMutationVariables>;
+
+/**
+ * __useSignInUserMutation__
+ *
+ * To run a mutation, you first call `useSignInUserMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useSignInUserMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [signInUserMutation, { data, loading, error }] = useSignInUserMutation({
+ *   variables: {
+ *      email: // value for 'email'
+ *      password: // value for 'password'
+ *   },
+ * });
+ */
+export function useSignInUserMutation(baseOptions?: Apollo.MutationHookOptions<SignInUserMutation, SignInUserMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<SignInUserMutation, SignInUserMutationVariables>(SignInUserDocument, options);
+      }
+export type SignInUserMutationHookResult = ReturnType<typeof useSignInUserMutation>;
+export type SignInUserMutationResult = Apollo.MutationResult<SignInUserMutation>;
+export type SignInUserMutationOptions = Apollo.BaseMutationOptions<SignInUserMutation, SignInUserMutationVariables>;
