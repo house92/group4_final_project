7aa374b36ac84b3324f6a821c66b2c01429a249a
MohammadAun <73369791+MohammadAun@users.noreply.github.com>
2024-02-23 10:32:25 -0600

    Final commit for friends details

---
+++ frontend/src/app/components/compounds/FriendIndex/FriendIndex.tsx
@@ -0,0 +1,20 @@
+import { Box } from '@mui/material';
+import FriendIndexItem from './FriendIndexItem';
+
+interface Friend {
+    name: string;
+}
+
+interface FriendsIndexItemProps {
+    friends: Friend[];
+}
+
+export default function FriendIndex({ friends }: FriendsIndexItemProps) {
+    return (
+        <Box>
+            {friends.map((friend, index) => (
+                <FriendIndexItem key={index} name={friend.name} />
+            ))}
+        </Box>
+    );
+};
