614c5fcdd6300bc81595c9e357039e255f6eea16
soph-amore <sophieamore19@gmail.com>
2024-02-24 11:20:52 -0600

    changes to author unit test

--- backend/src/authors/authors.service.spec.ts
+++ backend/src/authors/authors.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Author } from './author.entity';
 import { AuthorsService } from './authors.service';
 import { mock } from 'node:test';
+import { CreateAuthorInput } from './inputs/create-author.input';
 
 describe('AuthorsService', () => {
     let service: AuthorsService;
@@ -18,20 +19,38 @@ describe('AuthorsService', () => {
         expect(service).toBeDefined();
     });
 
-    it('should find all for id', () => {
+    it('should find all for authors', () => {
         const result = service.findAll();
-        expect(result).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    id: '1',
-                }),
-            ]),
-        );
+        expect(result).toBeDefined();
+        expect(Array.isArray(result)).toBe(true);
     });
 
-    it('should get all authors', () => {
-        const result = service.findAll();
-        expect(Array.isArray(result)).toEqual(true);
-        //expect(result.length).toEqual([service['authors'].length]);
+    it('should find author by id', async () => {
+        const authorId = '1';
+        const result = service.findById(authorId);
+        expect(result).toBeDefined();
+        expect((await result).id).toBe(authorId);
+    });
+
+    it('should create a new author', () => {
+        const CreateAuthorInput: Author = {
+            id: '1',
+            firstName: 'Mary',
+            lastName: 'Shelley',
+            dateOfBirth: '01-01-1800',
+        };
+        const createdAuthor = service.create(CreateAuthorInput);
+        expect(createdAuthor).toEqual(CreateAuthorInput);
+    });
+
+    it('should update an existing author', () => {
+        const existingAuthor: Author = { id: '1', firstName: 'Mary', lastName: 'Shelley', dateOfBirth: '01-01-1800' };
+        const updatedAuthorData = { firstName: 'William Shakespeare' };
+
+        service.create(existingAuthor);
+
+        const UpdateAuthorInput = service.update('1', updatedAuthorData);
+
+        expect(UpdateAuthorInput).toEqual({ existingAuthor, updatedAuthorData });
     });
 });
