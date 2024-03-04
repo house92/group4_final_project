2e5a47996529503577d2711cf9c411147f01589b
Sam <samuel.house@ntlworld.com>
2024-03-04 11:16:57 -0600

    added links to reviews

--- frontend/src/app/pages/HomePage/useHomePageData.ts
+++ frontend/src/app/pages/HomePage/useHomePageData.ts
@@ -1,12 +1,24 @@
-import { DateTime } from 'luxon';
-import { useGetHomePageDataQuery, GetHomePageDataQueryVariables } from 'generated/graphql';
+import { GetHomePageDataQuery, useGetHomePageDataQuery } from 'generated/graphql';
 
 interface Review {
     id: string;
     title: string;
+    bookHref: string;
     body: string;
     rating: number;
     reviewerName: string;
+    reviewerHref?: string;
+}
+
+function transformGqlBookReview(review: GetHomePageDataQuery['allReviews'][number]): Review {
+    return {
+        id: review.id,
+        title: review.book.title,
+        bookHref: `/books/${review.book.id}`,
+        body: review.body,
+        rating: review.rating,
+        reviewerName: `${review.user.firstName}`,
+    };
 }
 
 export default function useHomePageData(userId: string, authenticated: boolean) {
@@ -18,21 +30,12 @@ export default function useHomePageData(userId: string, authenticated: boolean) 
     if (data?.friendReviews?.friends) {
         reviews = data.friendReviews.friends.flatMap((friend) =>
             (friend.bookReviews || []).map((review) => ({
-                id: review.id,
-                title: review.book.title,
-                body: review.body,
-                rating: review.rating,
-                reviewerName: `${review.user.firstName} ${review.user.lastName}`,
+                ...transformGqlBookReview(review),
+                reviewerHref: `/users/${review.user.id}`,
             })),
         );
     } else if (data?.allReviews) {
-        reviews = data.allReviews.map((review) => ({
-            id: review.id,
-            title: review.book.title,
-            body: review.body,
-            rating: review.rating,
-            reviewerName: `${review.user.firstName}`,
-        }));
+        reviews = data.allReviews.map(transformGqlBookReview);
     }
     return { reviews };
 }
