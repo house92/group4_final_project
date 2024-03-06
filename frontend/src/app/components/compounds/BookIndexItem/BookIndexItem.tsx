2f772cc25e9cbc24a57e2ac5cd4078d181a8d55c
soph-amore <sophieamore19@gmail.com>
2024-03-05 20:26:06 -0600

    fix conflict

--- frontend/src/app/components/compounds/BookIndexItem/BookIndexItem.tsx
+++ frontend/src/app/components/compounds/BookIndexItem/BookIndexItem.tsx
@@ -1,4 +1,4 @@
-import { Pagination, Paper, Typography } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 import { DateTime } from 'luxon';
 
 interface BookIndexItemProps {
@@ -20,7 +20,6 @@ export default function BookIndexItem({ coverImage, title, authorNames, publicat
                     Publication Date: {publicationDate.toLocaleString(DateTime.DATETIME_SHORT)}
                 </Typography>
             )}
-            <Pagination count={10} size="small" />
         </Paper>
     );
 }
