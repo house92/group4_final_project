afb07f61d8d52aa369658923448cba781280816d
Duncan Beyer <duncan011217@gmail.com>
2024-03-10 17:17:21 -0500

    temp.

--- frontend/src/app/pages/UserPage/UserPage.tsx
+++ frontend/src/app/pages/UserPage/UserPage.tsx
@@ -4,12 +4,19 @@ import useUser, { useIsFriends, useIsInviteSentAlready, useIsInviteReceivedAlrea
 import { BookReviewIndex, UserDetails } from 'app/components';
 import { useSendFriendInviteMutation } from 'generated/graphql';
 import { useUserSession } from 'app/core/Session';
+import { useAcceptFriendInviteMutation } from 'generated/graphql';
 
-let sent: boolean = false;
+let disabled: boolean = false;
+let buttonFlag: boolean;
 
-function handleSend(func, userId) {
-    sent = true;
-    func({ variables: { userId } });
+function handleButton(sendInvite, acceptInvite, userId) {
+    if (!buttonFlag) {
+        disabled = true;
+        sendInvite({ variables: { userId } });
+    } else {
+        disabled = true;
+        acceptInvite({ variables: { userId } });
+    }
 }
 
 export default function UserPage() {
@@ -24,31 +31,56 @@ export default function UserPage() {
         myId = '';
     }
     const isFriends: boolean = useIsFriends(userId, myId);
+
     const isSent: boolean = useIsInviteSentAlready(userId, myId);
+    // ^^ true if the auth user has already sent friend request to the user whose page it is
+
     const isReceived: boolean = useIsInviteReceivedAlready(userId, myId);
+    // ^^ true if the user whose page it is has already sent friend request to the auth user
 
     const [sendInvite] = useSendFriendInviteMutation();
+    const [acceptInvite] = useAcceptFriendInviteMutation();
 
     if (!user) {
         return null;
     }
 
-    const doWeWantToPutAButtonToSendAFriendRequest: boolean =
-        !!userId && userId !== myId && !isFriends && !isSent && !isReceived;
+    let buttonText: string = 'Send Friend Request';
+
+    let putButton: boolean = true;
+
+    // True if button is to accept, false if button is to send
+
+    if (!!userId && userId !== myId) {
+        if (isSent) {
+            buttonText = 'Friend Request Already Sent';
+            disabled = true;
+        } else if (isReceived) {
+            buttonText = 'Accept Friend Request';
+            buttonFlag = true;
+        } else if (isFriends) {
+            putButton = false;
+        } else {
+            buttonText = 'Send Friend Request';
+            buttonFlag = false;
+        }
+    } else {
+        putButton = false;
+    }
 
     return (
         <Stack p={2} gap={4}>
             <UserDetails name={user?.name} age={user?.age} bio={user?.bio} />
-            {doWeWantToPutAButtonToSendAFriendRequest && (
+            {putButton && (
                 <Button
                     type="submit"
                     variant="contained"
                     onClick={() => {
-                        handleSend(sendInvite, userId);
+                        handleButton(sendInvite, acceptInvite, userId);
                     }}
-                    disabled={sent}
+                    disabled={disabled}
                 >
-                    Send Friend Request
+                    {buttonText}
                 </Button>
             )}
             <BookReviewIndex bookReviews={user.bookReviews} />
