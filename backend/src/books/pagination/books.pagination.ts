2165f7ef07cbd3e275b5779662902076a5ac5c17
Sam <samuel.house@ntlworld.com>
2024-03-09 19:00:29 -0600

    added title filter to listBooks

--- backend/src/books/pagination/books.pagination.ts
+++ backend/src/books/pagination/books.pagination.ts
@@ -1,12 +1,18 @@
-import { ArgsType, ObjectType } from '@nestjs/graphql';
+import { ArgsType, Field, ObjectType } from '@nestjs/graphql';
+import * as Joi from 'joi';
 import {
     ConnectionArgs,
+    ConnectionBuilderOptions,
+    Cursor,
+    OffsetCursorParameters,
     OffsetPaginatedConnectionBuilder,
     PageInfo,
     createConnectionType,
     createEdgeType,
+    validateParamsUsingSchema,
 } from 'nestjs-graphql-connection';
 import { Book } from '../book.entity';
+import { ListBooksFilter } from '../inputs/list-books.input';
 
 @ObjectType()
 export class BookEdge extends createEdgeType(Book) {}
@@ -19,10 +25,14 @@ export class BookConnectionArgs extends ConnectionArgs {
     /*
      * BookConnectionArgs will inherit `first`, `last`, `before`, `after`, and `page` fields from ConnectionArgs
      */
-    // EXAMPLE: Defining a custom argument for filtering
-    // @Field((type) => String, { nullable: true })
-    // public title?: string;
+    //EXAMPLE: Defining a custom argument for filtering
+    @Field(() => ListBooksFilter, { nullable: true })
+    public filter?: ListBooksFilter;
 }
+
+export type BookCursorParams = OffsetCursorParameters & { title?: string };
+
+export type BookCursor = Cursor<BookCursorParams>;
 
 export class BookConnectionBuilder extends OffsetPaginatedConnectionBuilder<
     BookConnection,
@@ -30,6 +40,18 @@ export class BookConnectionBuilder extends OffsetPaginatedConnectionBuilder<
     BookEdge,
     Book
 > {
+    public title?: string;
+
+    public constructor(connectionArgs: BookConnectionArgs, options: ConnectionBuilderOptions = {}) {
+        super(connectionArgs, options);
+
+        const { title } = connectionArgs.filter;
+
+        if (title) {
+            this.title = title;
+        }
+    }
+
     public createConnection(fields: { edges: BookEdge[]; pageInfo: PageInfo }): BookConnection {
         return new BookConnection(fields);
     }
@@ -38,6 +60,27 @@ export class BookConnectionBuilder extends OffsetPaginatedConnectionBuilder<
         return new BookEdge(fields);
     }
 
-    // When extending from OffsetPaginatedConnectionBuilder, cursor encoding/decoding always uses the OffsetCursor type.
-    // So it's not necessary to implement the createCursor() or decodeCursor() methods here.
+    public createCursor(node: Book, index: number): BookCursor {
+        const { title } = this.connectionArgs.filter;
+
+        const cursor: BookCursor = new Cursor({ offset: this.startOffset + index });
+
+        if (title) {
+            cursor.parameters.title = title;
+        }
+
+        return cursor;
+    }
+
+    public decodeCursor(encodedString: string): BookCursor {
+        return Cursor.fromString(encodedString, (params) =>
+            validateParamsUsingSchema(
+                params,
+                Joi.object({
+                    offset: Joi.number().integer().min(0).empty('').required(),
+                    title: Joi.string().optional(),
+                }).unknown(false),
+            ),
+        );
+    }
 }
